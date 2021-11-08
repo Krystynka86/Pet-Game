54c6477b60fa54de4ff0b1db2a9d37aec0769604
Krystynka86 <71220344+Krystynka86@users.noreply.github.com>
2021-11-08 18:49:48 +0100

    Initial Commit

---
+++ Pet Game/Pet_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pet_GameApp.swift
+//  Pet Game
+//
+//  Created by Cristina on 2021-11-08.
+//
+
+import SwiftUI
+
+@main
+struct Pet_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
